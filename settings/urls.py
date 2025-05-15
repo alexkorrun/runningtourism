d673c20324a18faa7a136a9fd94b1487d0824dfe
vellimir <kkazancov@gmail.com>
2025-05-15 23:21:58 +0300

    подключил маршруты к running

--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns: list[str] = [
     path("admin/", admin.site.urls),
+
+    path("", include("app.running.urls"))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
