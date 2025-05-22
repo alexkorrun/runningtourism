a5cbc9708c9a2289155264e6e7f058fa39edc680
vellimir <kkazancov@gmail.com>
2025-05-22 13:46:34 +0300

    обновил модели

--- app/running/admin.py
+++ app/running/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-
 from .models import Foto, Event, Feedback
 
-admin.site.register(Foto)
-admin.site.register(Event)
 
-admin.site.register(Feedback)
+@admin.register(Foto)
+class FotoAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("event_name", "event_year", "event_type", "event_view")
+    list_filter = ("event_view", "event_type", "event_year")
+    search_fields = ("event_name", "event_description")
+    date_hierarchy = "event_date1"
+    ordering = ("-event_year",)
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone")
+    search_fields = ("name", "phone")
+
 
 admin.site.site_title = "Админ-панель сайта Беговой Туризм"
 admin.site.site_header = "Админ-панель сайта Беговой Туризм"
--- mixins/str_mixin.py
+++ mixins/str_mixin.py
@@ -1,8 +1,9 @@
 class StrMixin:
     def __str__(self) -> str:
         if hasattr(self, "title") and self.title:
-            return self.title
+            return str(self.title)
         elif hasattr(self, "event_name") and self.event_name:
-            return self.event_name
+            return str(self.event_name)
         elif hasattr(self, "name") and self.name:
-            return self.name
+            return str(self.name)
+        return f"{self.__class__.__name__} object ({self.pk})"
